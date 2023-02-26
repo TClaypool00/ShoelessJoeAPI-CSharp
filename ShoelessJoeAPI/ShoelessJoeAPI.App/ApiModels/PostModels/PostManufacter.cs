2829d97c3d74c9f686753f1e47a4f3120d06db8d
claypooltyler00 <claypooltyler00@gmail.com>
2023-02-25 21:09:42 -0600

    Added model valiidation to model class

--- ShoelessJoeAPI/ShoelessJoeAPI.App/ApiModels/PostModels/PostManufacter.cs
+++ ShoelessJoeAPI/ShoelessJoeAPI.App/ApiModels/PostModels/PostManufacter.cs
@@ -1,8 +1,14 @@
-﻿namespace ShoelessJoeAPI.App.ApiModels.PostModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoelessJoeAPI.App.ApiModels.PostModels
 {
     public class PostManufacter
     {
+        [Required(ErrorMessage = "Manufacter name is required")]    
+        [MaxLength(255, ErrorMessage = "Manufacter name has max length of 255")]
         public string ManufacterName { get; set; }
+
+        [Required(ErrorMessage = "User Id is required")]
         public int UserId { get; set; }
     }
 }
