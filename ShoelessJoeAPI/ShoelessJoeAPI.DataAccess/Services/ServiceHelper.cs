380e8a8e012ab1de720e79abc854159951d03f26
claypooltyler00 <claypooltyler00@gmail.com>
2023-03-08 23:33:20 -0600

    Added service helper

---
+++ ShoelessJoeAPI/ShoelessJoeAPI.DataAccess/Services/ServiceHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace ShoelessJoeAPI.DataAccess.Services
+{
+    public class ServiceHelper
+    {
+        protected int _index;
+
+        public void ConfigureIndex(int? index)
+        {
+            if (index is null || (int)index == 1)
+            {
+                _index = 0;
+            }
+            else
+            {
+                _index = (int)index;
+            }
+        }
+    }
+}
