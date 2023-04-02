aa5a50482b483c3fda1f38db1bd9760c0886b412
claypooltyler00 <claypooltyler00@gmail.com>
2023-04-01 19:49:05 -0500

    API comment model

---
+++ ShoelessJoeAPI/ShoelessJoeAPI.App/ApiModels/ApiCommentModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoelessJoeAPI.App.ApiModels
+{
+    public class ApiCommentModel
+    {
+        public int CommentId { get; set; }
+        public string CommentText { get; set; }
+        public string Date { get; set; }
+
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
